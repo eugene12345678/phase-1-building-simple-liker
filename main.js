7c5c9930991302678a707f0b3b0c6bf6da6109ed
Eugene Mathenge <eugene.mukabi@student.moringaschool.com>
2024-07-10 19:32:03 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// Defining text characters for the empty and full hearts
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
-const hearts = document.querySelectorAll('.like-glyph');
-function toggleHeart(event) {
-  const heart = event.target;
-  
-  mimicServerCall()
-    .then(() => {
-      if (heart.innerText === EMPTY_HEART) {
-        heart.innerText = FULL_HEART;
-        heart.classList.add('activated-heart');
-      } else {
-        heart.innerText = EMPTY_HEART;
-        heart.classList.remove('activated-heart');
-      }
-    })
-    .catch((error) => {
-      const errorModal = document.getElementById('modal-error');
-      errorModal.innerText = error;
-      errorModal.classList.remove('hidden');
-      setTimeout(() => {
-        errorModal.classList.add('hidden');
-      }, 3000);
-    });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
 
-// Adding click event listeners to each heart
-hearts.forEach((heart) => {
-  heart.addEventListener('click', toggleHeart);
+  function toggleHeart(event) {
+    const heart = event.target;
+
+    mimicServerCall()
+      .then(() => {
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        const errorModal = document.getElementById('modal');
+        errorModal.innerText = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  }
+
+  // Adding click event listeners to each heart
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', toggleHeart);
+  });
 });
+
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
@@ -38,7 +41,7 @@ hearts.forEach((heart) => {
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < .2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
